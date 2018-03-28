c0a3e85c7d834c1374ff09f15b2d724dff8d5dbf
Tabakhase <mail@tabakhase.com>
2018-03-28 15:24:17 +0200

    (MAINT) fix paths when using DOCKER_TOOLBOX on windows
    Turns `D:/devstuff` into `/d/devstuff` when detecting DOCKER_TOOLBOX (docker inside virtualbox on windows)
    fixes `Docker::Error::ServerError: invalid volume specification: 'D:/devstuff:/tmp/stuff'`

--- lib/beaker/hypervisor/docker.rb
+++ lib/beaker/hypervisor/docker.rb
@@ -88,7 +88,12 @@ module Beaker
           unless host['mount_folders'].nil?
             container_opts['HostConfig'] ||= {}
             container_opts['HostConfig']['Binds'] = host['mount_folders'].values.map do |mount|
-              a = [ File.expand_path(mount['host_path']), mount['container_path'] ]
+              host_path = File.expand_path(mount['host_path'])
+              # When using docker_toolbox and getting a "(Driveletter):/" path, convert windows path to VM mount
+              if ENV['DOCKER_TOOLBOX_INSTALL_PATH'] && host_path =~ /^.\:\//
+                host_path = "/" + host_path.gsub(/^.\:/, host_path[/^(.)/].downcase)
+              end
+              a = [ host_path, mount['container_path'] ]
               a << mount['opts'] if mount.has_key?('opts')
               a.join(':')
             end
